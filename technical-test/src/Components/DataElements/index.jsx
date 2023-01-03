8811e98ae995d79897fe91ed65e4d0cb8e5f1805
Marc Moncusi Rubio <moncusimarc@gmail.com>
2023-01-03 14:10:38 +0100

    Part 1

---
+++ technical-test/src/Components/DataElements/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Element, Image, Name, Email } from './styles'
+
+const ListElement = ({ props }) => {
+  const picture = props.picture.medium
+  const name = props.name.first + ' ' + props.name.last
+  const email = props.email
+  return (
+    <Element>
+      <Image src={picture} alt={name} />
+      <Name> {name}</Name>
+      <Email> {email}</Email>
+    </Element>
+  )
+}
+
+export default ListElement
--- technical-test/src/Components/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react' 
-import { } from './styles'
-
-const ListElement = ({}) => {
-
-  return ()
-}
-
-export default ListElement

25a34fbf9c6ecbd54482840d02cd2d7b5b4a1a40
Marc Moncusi Rubio <moncusimarc@gmail.com>
2023-01-04 12:06:36 +0100

    reorder

---
+++ technical-test/src/Components/App/index.js
@@ -0,0 +1,19 @@
+import './styles'
+import { legacy_createStore as createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { UsersReducer } from '../Components/reducers/UsersReduce'
+import UsersList from '../Components/UsersList'
+import Buttons from '../Components/Button'
+
+const store = createStore(UsersReducer)
+
+function App () {
+  return (
+    <Provider store={store}>
+      <UsersList />
+      <Buttons />
+    </Provider>
+  )
+}
+
+export default App
